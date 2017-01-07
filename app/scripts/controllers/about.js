b24a983c76a98d8b19f52b2a70d48902372f7787
Wei-Chih Wang <Wei-Chih Wang>
2017-01-07 13:13:11 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demoAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the demoAppApp
+ */
+angular.module('demoAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
